210b2e90c873bdfe6aa0ba6d66ef76b0a6df3142
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2023-03-22 01:55:19 +0200

    Delete src directory

--- src/components/Layout/StyledLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material/styles';
-import Layout from './Layout';
-
-const StyledLayout = styled(Layout)`
-  height: 100vh;
-  /* background-color: ${props => props.theme.palette.primary.light}; */
-`;
-
-export default StyledLayout;
