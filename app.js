709e498fae39a204b788e5d8a636f0cf6264ab0e
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2023-03-22 01:57:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors());
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const dbUri = process.env.dbUri;
+const client = new MongoClient(dbUri);
+const collection = client.db("events").collection("events_data");
+
+app.get('/events', async (req, res) => {
+    let results = await dbReadAll();
+    res.send(results);
+});
+
+async function dbReadAll() {
+    await client.connect();
+    let all = await collection.find().toArray();
+    client.close();
+    return all;
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`app listening at port ${PORT}...`));
